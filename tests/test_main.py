455ff8b1a6b926b816d419e5355b41cbb0edd1db
Serhii Pirohov <semen4ik20@gmail.com>
2020-06-02 14:53:15 +0100

    fix observer galloper url and junit report name (#15)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from observer.app import create_parser, execute
@@ -8,13 +9,10 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 def test_main_with_data_params():
     args = create_parser().parse_args(
         [
-            "-f", f"data.zip",
+            #"-f", f"data.zip",
             # "-d", f"{ROOT_DIR}/data/data.json",
             "-sc", "/tmp/data/webmail.side",
-            "-fp", "100",
-            "-si", "400",
-            "-tl", "500",
             "-r", "html",
-            # "-g", "False"
+            "-g", "False"
         ])
     execute(args)
