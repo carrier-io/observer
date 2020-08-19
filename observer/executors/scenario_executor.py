0c7a6e0128884a71f403f3ed1176c556b9d65eb9
SergeyPirogov <semen4ik20@gmail.com>
2020-08-19 13:06:11 +0300

    clean up code

--- observer/executors/scenario_executor.py
+++ observer/executors/scenario_executor.py
@@ -1,34 +1,17 @@
 from observer.executors.command_executor import execute_command
 from observer.processors.test_data_processor import get_test_data_processor
-from observer.util import _pairwise, logger, flatten_list
+from observer.util import _pairwise, logger
 
 
 def execute_scenario(scenario, args):
-    # test_results = []
     for test in scenario['tests']:
         logger.info(f"Executing test: {test['name']}")
-        results = _execute_test(test, args)
-        # test_results.append(results)
-    # return flatten_list(test_results)
+        _execute_test(test, args)
 
 
 def _execute_test(test, args):
     test_name = test['name']
-    # locators = []
-
     test_data_processor = get_test_data_processor(test_name, args.data)
+    for current_command in test['commands']:
+        execute_command(current_command, test_data_processor)
 
-    # results = []
-    for current_command, next_command in _pairwise(test['commands']):
-        execution_result = execute_command(current_command, next_command, test_data_processor, args.video)
-        # locators.append(current_command)
-        # if execution_result.ex:
-        #     break
-        #
-        # if not execution_result.is_ready_for_report():
-        #     continue
-        #
-        # execution_result.locators = locators.copy()
-        # results.append(execution_result)
-
-    # return results
--- tests/test_main.py
+++ tests/test_main.py
@@ -21,29 +21,3 @@ def test_main_with_data_params():
             "-a", "max"
         ])
     execute(args)
-
-
-def test_jira_reporter():
-    jira = JiraClient("http://192.168.0.107:8088", "SergeyPirogov", "123456", "DEMO")
-    # jira.create_issue("Threshold: all [Total] min value 200 violates rule lte 50", "High", "")
-    # issues = jira.get_issues()
-    issues = jira.get_issues("75f58c287ddcc1b1eef9ebc85906216c2b6a25b3b9310cfb632de0571ce98c32")
-    print(issues)
-
-
-def test_ado():
-    personal_access_token = 'q3se3d23xq6tiaisrz3lp76edvegbmvysvaopyqvhtsaqb7dweza'
-    ado = AdoClient('SerhiiPirohov', 'DEMO', personal_access_token, issue_type="issue")
-    res = ado.create_issues({}, [])
-    print(res)
-
-
-def test_ff():
-    from selenium import webdriver
-    ff_options = webdriver.FirefoxOptions()
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        options=ff_options)
-
-    driver.get("http://automation-remarks.com")
-    driver.close()
