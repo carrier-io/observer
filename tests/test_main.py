997f10a8bcd4c44d68d3b05ee1ecea043559a0c0
Serhii Pirohov <semen4ik20@gmail.com>
2020-08-19 10:50:52 +0000

    Clean up perf logic (#34)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 import os
 
 from observer.app import create_parser, execute
+from observer.reporters.azure_devops import AdoClient
+from observer.reporters.jira_reporter import JiraClient
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,12 +10,14 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 def test_main_with_data_params():
     args = create_parser().parse_args(
         [
-            "-f", f"data.zip",
+            # "-f", f"data.zip",
             # "-d", f"{ROOT_DIR}/data/data.json",
             "-sc", "/tmp/data/webmail.side",
-            # "-g", "False",
-            # "-v", "False",
+            "-g", "False",
+            "-v", "False",
+            # "-r", "ado",
             "-l", "1",
+            "-b", "chrome",
             "-a", "max"
         ])
     execute(args)
